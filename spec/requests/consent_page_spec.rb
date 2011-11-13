28abf2b1706cc43106b9bf1568e8be4ce0a945cf
Tim Booher <ads@theboohers.org>
2011-11-13 14:22:16 -0500

    sunday school work

---
+++ spec/requests/consent_page_spec.rb
@@ -0,0 +1,21 @@
+require File::expand_path('../../spec_helper', __FILE__)
+
+#Does your physician approve of your participation in this exercise program?
+# - must answer yes or text in explanation field
+#Are you taking any medications that affect your vital signs (heart rate, blood pressure, breathing, etc.) or physical performance?
+# - if yes, must enter text in explanation field
+#Are you a post-menopausal female?
+# - ?? need logic here
+#If yes, are you taking estrogen?
+# - ?? must check
+
+# and the end off to: /registration/release_and_waiver_of_liability
+
+describe "The user consent page" do
+
+  it "should prevent sick people from screwing things up" do
+
+    #To change this template use File | Settings | File Templates.
+    true.should == false
+  end
+end
