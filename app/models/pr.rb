19e392835d221c28ce552033f394d5b6c8d6f44f
Tim Booher <tim@theboohers.org>
2011-11-15 22:28:05 -0500

    updated

--- app/models/pr.rb
+++ app/models/pr.rb
@@ -1,7 +1,16 @@
 class Pr
   include Mongoid::Document
+  include Mongoid::Timestamps
+
   field :score, :type => String
   field :user_note, :type => String
   field :rxd, :type => Boolean
   field :common_value, :type => Float
+
+  # relations
+  embedded_in :user
+  belongs_to :workout
+  belongs_to :fit_wit_workout
+
 end
+
