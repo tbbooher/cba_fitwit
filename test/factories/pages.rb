b6ab96744b695d607d261120b5501338dc9efe6c
Tim Booher <ads@theboohers.org>
2011-11-16 07:20:23 -0500

    updated factory girls

--- test/factories/pages.rb
+++ test/factories/pages.rb
@@ -1,24 +1,25 @@
-Factory.define :page do |p|
-   p.title                 'Page 1'
-   p.body                  "Lorem ipsum"
-   p.show_in_menu          true
-   p.allow_comments        true
-   p.allow_public_comments true
-   p.allow_removing_component true
-   p.is_draft              false
-   p.interpreter           :markdown
+FactoryGirl.define do
+factory :page do
+   title                 'Page 1'
+   body                  "Lorem ipsum"
+   show_in_menu          true
+   allow_comments        true
+   allow_public_comments true
+   allow_removing_component true
+   is_draft              false
+   interpreter           :markdown
 end
 
-Factory.define :page_with_default_template, :class => Page do |p|
-  p.title                 'Page 1'
-  p.body                  "Lorem ipsum"
-  p.show_in_menu          true
-  p.allow_comments        true
-  p.allow_public_comments true
-  p.allow_removing_component true
-  p.is_draft              false
-  p.interpreter           :markdown
-  p.page_template         PageTemplate.find_or_create_by(:name => 'default', :html_template => "TITLE BODY COMPONENTS COMMENTS BUTTONS")
+factory :page_with_default_template, :class => Page do
+  title                 'Page 1'
+  body                  "Lorem ipsum"
+  show_in_menu          true
+  allow_comments        true
+  allow_public_comments true
+  allow_removing_component true
+  is_draft              false
+  interpreter           :markdown
+  page_template         PageTemplate.find_or_create_by(:name => 'default', :html_template => "TITLE BODY COMPONENTS COMMENTS BUTTONS")
 end
-
+end
 
--- test/factories/postings.rb
+++ test/factories/postings.rb
@@ -1,9 +1,11 @@
-Factory.define :posting do |p|
-   p.title      'Posting 1'
-   p.body       "Lorem ipsum"
-   p.user_id    "4d2c96042d194751eb000001"
-   p.blog_id    "4d2c96042d194751eb000002"
-   p.is_draft   false
+FactoryGirl.define do
+factory :posting do 
+   title      'Posting 1'
+   body       "Lorem ipsum"
+   user_id    "4d2c96042d194751eb000001"
+   blog_id    "4d2c96042d194751eb000002"
+   is_draft   false
+end
 end
 
 
