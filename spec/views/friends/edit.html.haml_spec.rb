35260ae2892fe187d14f3d229d04860665e8ad63
Tim Booher <ads@theboohers.org>
2011-11-20 21:39:12 -0500

    time_slots spec working

---
+++ spec/views/friends/edit.html.haml_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "friends/edit.html.haml" do
+  before(:each) do
+    @friend = assign(:friend, stub_model(Friend,
+      :name => "MyString"
+    ))
+  end
+
+  it "renders the edit friend form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form", :action => friends_path(@friend), :method => "post" do
+      assert_select "input#friend_name", :name => "friend[name]"
+    end
+  end
+end
