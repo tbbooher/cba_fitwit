961602e8a30d6c21169ce1065ce5fa6c088f6a6a
Tim Booher <tim@theboohers.org>
2011-11-21 22:12:35 -0500

    got to step 2

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -32,3 +32,5 @@ omniauth_file =  File.expand_path('../../config/omniauth_settings.yml', __FILE__
 if File.exist?(omniauth_file)
   YAML.load_file(omniauth_file).each{|k,v| ENV[k.to_s] = v.to_s}
 end
+
+PRICE = YAML.load_file("config/prices.yml")
