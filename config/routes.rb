274f52dcf36f1ce78c450e02cb1c257b7b459baa
Tim Booher <tim@theboohers.org>
2011-11-24 06:48:38 -0500

    removed time_slot_spec~

--- config/routes.rb
+++ config/routes.rb
@@ -3,77 +3,42 @@
 Cba::Application.routes.draw do
 
   get "my_fit_wit/index"
-
   get "my_fit_wit/profile"
-
   get "my_fit_wit/upcoming_fitnesscamps"
-
   get "my_fit_wit/add_custom_workout"
-
   get "my_fit_wit/input_custom_workout"
-
   get "my_fit_wit/leader_board"
-
   get "my_fit_wit/get_progress_chart"
-
   get "my_fit_wit/exercise_details"
-
   get "my_fit_wit/update"
-
   get "my_fit_wit/past_fitnesscamps"
-
   get "my_fit_wit/camp_exercise_progress"
-
   get "my_fit_wit/my_goals"
-
   get "my_fit_wit/add_goal"
-
   get "my_fit_wit/delete_goal"
-
   get "my_fit_wit/update_goal"
-
   get "my_fit_wit/exercise_progress"
-
   get "my_fit_wit/add_new_measurement"
-
   get "my_fit_wit/load_calendar_date"
-
   get "my_fit_wit/specific_exercise"
-
   get "my_fit_wit/process_fit_wit_history"
-
   get "my_fit_wit/health_history"
-
   get "my_fit_wit/get_calendar_events"
-
   get "my_fit_wit/list_exercise"
-
   get "my_fit_wit/find_previous_scores"
-
   get "my_fit_wit/get_user_id"
-
   get "my_fit_wit/zero_out_all_unchecked_explanations"
-
   get "my_fit_wit/user_has_not_explained_themself"
-
   get "my_fit_wit/get_inches_height"
-
   get "my_fit_wit/get_months_and_years"
 
   resources :friends
-
   resources :prs
-
   resources :fit_wit_workouts
-
   resources :workouts
-
   resources :meetings
-
   resources :locations
-
   resources :sponsors
-
   resources :events
 
   get "calendar/index"
@@ -82,19 +47,19 @@ Cba::Application.routes.draw do
 
   # FitWit Routes
 
-   get "base/locations", as: "locations"
-   get "base/camp_details", as: "camp_details"
-   get "base/whats_included", as: "whats_included"
-   get "base/faq", as: "faq"
-   get "base/price", as: "price"
-   get "base/schedule", as: "schedule"
-   get "base/non_profit", as: "non_profit"
-   get "base/stories", as: "stories"
-   get "base/company_story", as: "company_story"
-   get "base/camper_stories", as: "camper_stories"
-   get "base/contact", as: "contact"
-   get 'base/error'
-   get 'base/posts'
+  get "base/locations", as: "locations"
+  get "base/camp_details", as: "camp_details"
+  get "base/whats_included", as: "whats_included"
+  get "base/faq", as: "faq"
+  get "base/price", as: "price"
+  get "base/schedule", as: "schedule"
+  get "base/non_profit", as: "non_profit"
+  get "base/stories", as: "stories"
+  get "base/company_story", as: "company_story"
+  get "base/camper_stories", as: "camper_stories"
+  get "base/contact", as: "contact"
+  get 'base/error'
+  get 'base/posts'
 
   # fitness camp registrations
   get 'fitness_camp_registration/index'
@@ -117,7 +82,7 @@ Cba::Application.routes.draw do
 
   # Switch locales
   match 'switch_lcoale/:locale' => "home#set_locale", :as => 'switch_locale'
-  
+
   # Switch draft mode
   match 'draft_mode/:mode' => "home#set_draft_mode", :as => 'draft_mode'
 
@@ -125,7 +90,7 @@ Cba::Application.routes.draw do
 
   # Comments
   resources :comments, :except => :show
-  
+
   # Tags
   match '/tag/:tag' => "home#tags", :as => 'tags'
 
@@ -165,9 +130,9 @@ Cba::Application.routes.draw do
       post :sort_components
     end
     collection do
-      get  :new_article
+      get :new_article
       post :create_new_article
-      get  :templates
+      get :templates
     end
     resources :comments
     resources :page_components
@@ -181,11 +146,11 @@ Cba::Application.routes.draw do
   match 'hide_notification/:id' => 'users#hide_notification', :as => 'hide_notification'
   match 'show_notification/:id' => 'users#show_notification', :as => 'show_notification'
   match 'notifications' => 'users#notifications', :as => 'notifications'
-  match 'profile/:id'   => 'users#show', :as => 'profile'
+  match 'profile/:id' => 'users#show', :as => 'profile'
 
-  devise_for :users, :controllers => { :registrations => 'registrations' }
-  resources :users, :only => [:show,:destroy] do
-    resources :invitations    
+  devise_for :users, :controllers => {:registrations => 'registrations'}
+  resources :users, :only => [:show, :destroy] do
+    resources :invitations
     resources :user_groups
     resources :user_notifications
     member do
@@ -202,7 +167,7 @@ Cba::Application.routes.draw do
 
   # AUTHENTICATIONS
   match '/auth/:provider/callback' => 'authentications#create'
-  resources :authentications, :only => [:index,:create,:destroy]
+  resources :authentications, :only => [:index, :create, :destroy]
   match '/auth/failure' => 'authentications#auth_failure'
 
   resources :user_notifications do
