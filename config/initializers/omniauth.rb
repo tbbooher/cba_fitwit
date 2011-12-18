3e62280d35f31138b552552d043fb72001ec871f
Tim Booher <tim@theboohers.org>
2011-12-18 14:30:01 -0500

    added twitter and facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,9 +4,9 @@
 #require File.join(Rails.root,'config/omniauth_settings')
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV['OMNIAUTH_GITHUB_ID'], ENV['OMNIAUTH_GITHUB_SECRET']
-  provider :google_oauth2, ENV['OMNIAUTH_GOOGLE_KEY'], ENV['OMNIAUTH_GOOGLE_SECRET']
+  #provider :google_oauth2, ENV['OMNIAUTH_GOOGLE_KEY'], ENV['OMNIAUTH_GOOGLE_SECRET']
   provider :facebook, ENV['OMNIAUTH_FACEBOOK_ID'], ENV['OMNIAUTH_FACEBOOK_SECRET']
-  provider :twitter, ENV['OMNIAUTH_TWITTER_ID'], ENV['OMNIAUTH_TWITTER_SECRET']
+  provider :twitter, ENV['OMNIAUTH_TWITTER_CONSUMER_KEY'], ENV['OMNIAUTH_TWITTER_SECRET']
 end
 
 
