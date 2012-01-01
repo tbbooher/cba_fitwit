aaf6a15c22effafb28a6f09380a42448efc4a94f
Tim Booher <ads@theboohers.org>
2012-01-01 11:24:21 -0500

    all together

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -13,12 +13,17 @@ else
   USER_DATABASE={ 'use_remote_database' => false }
 end
 
-config_file =  File.expand_path('../../config/application.yml', __FILE__)
+config_file = File.expand_path('../../config/application.yml', __FILE__)
 if File.exist?(config_file)
   APPLICATION_CONFIG=YAML.load_file(config_file)[Rails.env]['application']
   CONSTANTS=YAML.load_file(config_file)[Rails.env]['constants']
   APPLICATION_CONFIG.each{|k,v| ENV["APPLICATION_CONFIG_#{k}"] = v.to_s }
   CONSTANTS.each{|k,v| ENV["CONSTANTS_#{k}"] = v.to_s}
+end
+
+price_file = File.expand_path('../../config/prices.yml', __FILE__)
+if File.exist?(price_file)
+  PRICES = YAML.load_file(price_file)
 end
 
 # MAILSERVER settings
