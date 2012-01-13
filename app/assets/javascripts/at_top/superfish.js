36a515b1a52dc0ffc4989909e6f0457feb49f7d3
Andrew Sharp <andrew@sharpteam.org>
2012-01-13 00:30:33 -0500

    superfish.js no real change

--- app/assets/javascripts/at_top/superfish.js
+++ app/assets/javascripts/at_top/superfish.js
@@ -15,7 +15,6 @@
 
 		var sf = $.fn.superfish,
 			c = sf.c,
-			$arrow = $(['<span class="',c.arrowClass,'"> &#187;</span>'].join('')),
 			over = function(){
 				var $$ = $(this), menu = getMenu($$);
 				clearTimeout(menu.sfTimer);
