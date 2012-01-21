91e02c4376c40e848d987b0c1e48a22fd4e3d7e4
Tim Booher <tim@theboohers.org>
2012-01-21 10:12:59 -0500

    removed some code

--- app/models/order.rb
+++ app/models/order.rb
@@ -15,8 +15,8 @@ class Order
 
   has_many :registrations, :dependent => :destroy
   has_many :order_transactions,
-    :class_name => 'OrderTransaction', 
-    :dependent => :destroy
+           :class_name => 'OrderTransaction',
+           :dependent => :destroy
   belongs_to :user
   # belongs_to :coupon_code # , :counter_cache => :uses
   # serialize :params TODO -- fix this
@@ -25,9 +25,9 @@ class Order
   # field :array_params, type: Array
 
   cattr_accessor :gateway
-  
+
   # ...
-  
+
   #  PAYMENT_TYPES = [
   #    #  Displayed        stored in db
   #    [ "Check",          "check" ],
@@ -37,17 +37,18 @@ class Order
   # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
   # validations
   # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-  
+
   validates_presence_of :user_id, :amount, :description
   validates_numericality_of :amount
   #validates_format_of :expiration_mo, :with => /^\d{2}$/, :message => "must be two digit year"
   #validates_inclusion_of :payment_method, :in => PAYMENT_TYPES.map {|disp, value| value}
   def self.card_types
-    { 'Visa'             => 'visa',
-      'MasterCard'       => 'master',
-      'Discover'         => 'discover',
-      'American Express' => 'american_express' }
+    {'Visa' => 'visa',
+     'MasterCard' => 'master',
+     'Discover' => 'discover',
+     'American Express' => 'american_express'}
   end
+
   #  cardtypes = [ :visa, :master, :american_express, :diners_club, :jcb, :switch, :solo, :maestro ]
   # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
   # FROM PEEPCODE
@@ -91,6 +92,7 @@ class Order
       authorization.reference
     end
   end
+
   # END authorization_reference
 
   # BEGIN number
@@ -100,6 +102,7 @@ class Order
     mydate = Date.today().strftime('%Y%m%d')
     "#{self.user.last_name}_#{mydate}_#{self.id}"
   end
+
   # END number
 
   # BEGIN authorize_payment
@@ -107,56 +110,58 @@ class Order
 
     # this is the major function that interacts with the credit card company
     options[:order_id] = number # currently just loading a date
-    #    options[:email] = 'developer@fitwit.com'
-    #    options[:billing_address] = {
-    #      :name => 'Cody Fauser',
-    #      :address1 => '1234 Shady Brook Lane',
-    #      :address2 => 'Apartment 1',
-    #      :city => 'Ottawa',
-    #      :state => 'ON',
-    #      :country => 'CA',
-    #      :zip => 'K3P5N5',
-    #      :phone => '999-999-9999'}
+                                #    options[:email] = 'developer@fitwit.com'
+                                #    options[:billing_address] = {
+                                #      :name => 'Cody Fauser',
+                                #      :address1 => '1234 Shady Brook Lane',
+                                #      :address2 => 'Apartment 1',
+                                #      :city => 'Ottawa',
+                                #      :state => 'ON',
+                                #      :country => 'CA',
+                                #      :zip => 'K3P5N5',
+                                #      :phone => '999-999-9999'}
 
-    #transaction do
+                                #transaction do
 
-    # amount is a property of the order
+                                # amount is a property of the order
 
-      authorization = OrderTransaction.authorize(amount, credit_card, options)
-      self.save!
-      self.order_transactions << authorization
+    authorization = OrderTransaction.authorize(amount, credit_card, options)
+    self.save!
+    self.order_transactions << authorization
 
-      if authorization.success?
-        self.payment_authorized!
-      else
-        self.transaction_declined!
-      end
+    if authorization.success?
+      self.payment_authorized!
+    else
+      self.transaction_declined!
+    end
 
-      # testing
+    # testing
 
-      #self.order_transactions.reload
+    #self.order_transactions.reload
 
-      authorization
+    authorization
     #end
   end
+
   # END authorize_payment
-  
+
   # BEGIN capture_payment
   def capture_payment(options = {})
-   # transaction do
-      capture = OrderTransaction.capture(amount, authorization_reference, options)
-      self.save!
-      self.order_transactions << capture
+    # transaction do
+    capture = OrderTransaction.capture(amount, authorization_reference, options)
+    self.save!
+    self.order_transactions << capture
 
-      if capture.success?
-        self.payment_captured!
-      else
-        self.transaction_declined!
-      end
+    if capture.success?
+      self.payment_captured!
+    else
+      self.transaction_declined!
+    end
 
-      capture
-   # end
+    capture
+    # end
   end
+
   # END capture_payment
 
   # foray into the world of subscriptions (profiles)
@@ -165,17 +170,17 @@ class Order
     u = self.user
     options[:order_id] = number # currently just loading a date
     options[:email] = u.email
-    options[:address] =  {
-                :first_name => u.first_name,
-                :last_name => u.last_name,
-                :address1 => u.street_address1,
-                :address2 => (u.street_address2 || ""),
-                :company => (u.company || ""),
-                :city => u.city,
-                :state => u.us_state,
-                :zip => u.zip,
-                :country => 'United States',
-                :phone => u.primary_phone
+    options[:address] = {
+        :first_name => u.first_name,
+        :last_name => u.last_name,
+        :address1 => u.street_address1,
+        :address2 => (u.street_address2 || ""),
+        :company => (u.company || ""),
+        :city => u.city,
+        :state => u.us_state,
+        :zip => u.zip,
+        :country => 'United States',
+        :phone => u.primary_phone
     }
 
     subscription = OrderTransaction.generate_yearly_subscription(credit_card, options)
@@ -194,11 +199,11 @@ class Order
   # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
   # END FROM PEEPCODE
   # |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-  
+
   def register_timeslots_from_cart(cart)
     cart.items.each do |item|
       rs = Registration.create(:time_slot_id => item.timeslot.id,
-        :order_id => self.id)
+                               :order_id => self.id)
       #        :name_of_friend => item.name_of_friend,
       #        :discount_category => item.discount_category)
       # now add all friends to this registration
@@ -206,14 +211,88 @@ class Order
         rs.friends << Friend.create(:name => friend)
       end # friends
       rs.save
-      registrations << rs      
+      registrations << rs
     end # items
   end
-  
+
   def register_user_to_time_slot(time_slot_id)
     Registration.create(:time_slot_id => time_slot_id,
-      :order_id => self.id)
+                        :order_id => self.id)
     #        :name_of_friend => '',
     #        :discount_category => 0)
   end
+
+  def complete_purchase(cart, params, user)
+    credit_card = ActiveMerchant::Billing::CreditCard.new(params[:credit_card])
+    if credit_card.valid?
+      if authorize(cart) == "success"
+
+      else
+        # handle credit card errors flash notice
+      end
+    end
+  end
+
+  private
+
+  def authorize(cart)
+    if cart.new_membership
+      if @order.create_subscription(@credit_card, options).success?
+        "success"
+      else
+        "subscription error"
+      end
+    else
+      if self.authorize_payment(@credit_card, options).success?
+        "success"
+      else
+        purchase.message + "<br>" + purchase.params['missingField'].to_s
+      end
+    end
+  end
+
+  def send_emails(is_membership, user, order,cart)
+    unless is_membership
+      inform_management = Postman.create_new_order(user,
+        order,
+        params[:credit_card],
+        session[:health_approval],
+        cart)
+      inform_customer = Postman.create_inform_customer(user,
+        order,
+        params[:credit_card],
+        session[:health_approval],
+        cart)
+    else
+      inform_management = Postman.create_inform_ben_membership(user,
+        order,
+        params[:credit_card],
+        session[:health_approval],
+        cart)
+      inform_customer = Postman.create_inform_user_membership(user,
+        order,
+        params[:credit_card],
+        session[:health_approval],
+        cart)
+    end
+    Postman.deliver(inform_management)
+    Postman.deliver(inform_customer)
+  end
+
+  def build_options(u, state = nil, zip = nil, city = nil, address1 = nil, address2 = nil)
+    # this is my attempt at populating the options hash for the credit card
+    options = {
+      :email => u.email,
+      :billing_address => {
+        :name => u.full_name,
+        :address1 => address1 || u.street_address1,
+        :address2 => address2 || u.street_address2,
+        :city => city || u.city,
+        :state => state || u.us_state,
+        :country => 'US',
+        :zip => zip || u.zip,
+        :phone => u.primary_phone}
+    }
+  end
+
 end
