2160dce728f0c4a53eafbcb18d202aace391445f
Tim Booher <ads@theboohers.org>
2012-01-23 20:10:08 -0500

    starting calendar

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,11 +9,10 @@ class Event
   field :all_day, :type => Boolean
   field :description, :type => String
 
-  #scope :before2, lambda {|end_time| {:conditions => ["ends_at < ?", Event.format_date(end_time)] }}
-  #scope :after2, lambda {|start_time| {:conditions => ["starts_at > ?", Event.format_date(start_time)] }}
+  validate :dates_correlation
 
-  scope :before, lambda {|end_time| where(:ends_at.lt => Event.format_date(end_time))}
-  scope :after, lambda {|start_time| where(:ends_at.gt => Event.format_date(start_time))}
+  scope :before, lambda {|end_time| {:conditions => ["ends_at < ?", Event.format_date(end_time)] }}
+  scope :after, lambda {|start_time| {:conditions => ["starts_at > ?", Event.format_date(start_time)] }}
 
   # need to override the json view to return what full_calendar is expecting.
   # http://arshaw.com/fullcalendar/docs/event_data/Event_Object/
@@ -33,6 +32,10 @@ class Event
 
   def self.format_date(date_time)
     Time.at(date_time.to_i).to_formatted_s(:db)
+  end
+
+  def dates_correlation
+    @errors.add(:ends_at, "The event must end after it started") if starts_at > ends_at
   end
 
 end
