ea97839707c3ea2a377069bb5a66f94e020af14a
Tim Booher <ads@theboohers.org>
2012-01-24 22:00:42 -0500

    calendar working

--- app/assets/javascripts/backend/calendar.js
+++ app/assets/javascripts/backend/calendar.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 
-	var date = new Date();
-	var d = date.getDate();
-	var m = date.getMonth();
-	var y = date.getFullYear();
-	
-	$('#calendar').fullCalendar({
-		editable: true,        
-		header: {
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+    var location_id = window.location.pathname.split("/")[3];
+
+    $('#calendar').fullCalendar({
+        editable: true,
+        header: {
             left: 'prev,next today',
             center: 'title',
             right: 'month,agendaWeek,agendaDay'
@@ -15,51 +16,56 @@ $(document).ready(function() {
         defaultView: 'month',
         height: 500,
         slotMinutes: 15,
-        
-        loading: function(bool){
-            if (bool) 
+
+        loading: function(bool) {
+            if (bool)
                 $('#loading').show();
-            else 
+            else
                 $('#loading').hide();
         },
-        
+
         // a future calendar might have many sources.        
-        eventSources: [{
-            url: '/backend/events',
-            color: 'yellow',
-            textColor: 'black',
-            ignoreTimezone: false
-        }],
-        
+        eventSources: [
+            {
+                url: '/backend/locations/' + location_id + '/events',
+                color: '#efefef;',
+                textColor: 'black',
+                ignoreTimezone: false
+            }
+        ],
+
         timeFormat: 'h:mm t{ - h:mm t} ',
         dragOpacity: "0.5",
-        
+
         //http://arshaw.com/fullcalendar/docs/event_ui/eventDrop/
-        eventDrop: function(event, dayDelta, minuteDelta, allDay, revertFunc){
+        eventDrop: function(event, dayDelta, minuteDelta, allDay, revertFunc) {
             updateEvent(event);
         },
 
         // http://arshaw.com/fullcalendar/docs/event_ui/eventResize/
-        eventResize: function(event, dayDelta, minuteDelta, revertFunc){
+        eventResize: function(event, dayDelta, minuteDelta, revertFunc) {
             updateEvent(event);
         },
 
         // http://arshaw.com/fullcalendar/docs/mouse/eventClick/
-        eventClick: function(event, jsEvent, view){
-          // would like a lightbox here.
-        },
-	});
+        eventClick: function(event, jsEvent, view) {
+            // would like a lightbox here.
+        }
+    });
 });
 
 function updateEvent(the_event) {
     $.update(
-      "/events/" + the_event.id,
-      { event: { title: the_event.title,
-                 starts_at: "" + the_event.start,
-                 ends_at: "" + the_event.end,
-                 description: the_event.description
-               }
-      },
-      function (reponse) { alert('successfully updated task.'); }
+        "/events/" + the_event.id,
+        { event: { title: the_event.title,
+            starts_at: "" + the_event.start,
+            ends_at: "" + the_event.end,
+            description: the_event.description
+        }
+        },
+        function (reponse) {
+            alert('successfully updated task.');
+        }
     );
-};
+}
+;
