7475ae258512c19112f0322b7eb5a97929ac24f7
Tim Booher <ads@theboohers.org>
2012-02-04 14:08:02 -0500

    FitWit

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,6 +1,6 @@
 if Rails.env == "production"
   # set credentials from ENV hash
-  S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => "FitWit"}
+  S3_CREDENTIALS = { :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET'], :bucket => "FitWitSite"}
 else
   # get credentials from YML file
   S3_CREDENTIALS = Rails.root.join("config/s3.yml")
