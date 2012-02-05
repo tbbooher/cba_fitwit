cf166003fbf72bd6f4c1ecac87b89a0325f01e5e
Tim Booher <ads@theboohers.org>
2012-02-05 08:58:46 -0500

    updated user sorting

--- app/controllers/backend/users_controller.rb
+++ app/controllers/backend/users_controller.rb
@@ -1,3 +1,15 @@
 class Backend::UsersController < Backend::ResourceController
-
+  def index
+    @locations = Location.all
+    @location_id = params[:location]
+    if @location_id
+      if @location_id == 'None'
+        @users = User.where(location_id: nil).page params[:page]
+      else
+        @users = User.where(location_id: @location_id).page params[:page]
+      end
+    else
+      @user = User.all.page params[:page]
+    end
+  end
 end
--- app/helpers/backend/application_helper.rb
+++ app/helpers/backend/application_helper.rb
@@ -2,4 +2,10 @@ module Backend::ApplicationHelper
   def odd_or_even
     { class: cycle("odd", "even", name: "rows") }
   end
+
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
+    link_to title, :sort => column, :direction => direction
+  end
 end
