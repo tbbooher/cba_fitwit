475c02bdd89a4ba2b67a31cfe8aea92304057c51
Tim Booher <ads@theboohers.org>
2012-03-25 20:58:52 -0400

    updated

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -2,9 +2,9 @@ class Ckeditor::Picture < Ckeditor::Asset
   has_mongoid_attached_file :data,
                             :storage => :s3,
                             :s3_credentials => S3_CREDENTIALS,
-                            :url  => ":s3_domain_url",
                             :path => ":attachment/:id/:style_:basename.:extension",
-                            :styles => { :content => '800>', :thumb => '118x100#' }
+                            :styles => { :content => '800>', :thumb => '118x100#' },
+                            :default_url => "/images/avatars/:style/missing.png"
 
   validates_attachment_size :data, :less_than => 2.megabytes
   validates_attachment_presence :data
