34d101676bd0f2f9168e42d2ca3ed4dd134797d6
Tim Booher <ads@theboohers.org>
2012-04-14 12:04:01 -0400

    updated emails

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -11,8 +11,13 @@ class Notifications < ActionMailer::Base
   # When a new user signed up, inform the adminstrator
   def sign_up(new_user)
     @user = new_user
-    @notify_subject = strip_tags "NEW SIGN UP AT #{APPLICATION_CONFIG['name']}"
+    @notify_subject = strip_tags "NEW SIGN UP AT #{ENV['APPLICATION_CONFIG_name']}"
     mail( :to => ENV['APPLICATION_CONFIG_admin_notification_address'], :subject => @notify_subject)
+  end
+
+  def new_user(user)
+    @user = user
+    mail( to: "info@fitwit.com", from: "info@fitwit.com", subject: "New user #{@user.full_name}" )
   end
 
   def account_created(user)
@@ -24,13 +29,13 @@ class Notifications < ActionMailer::Base
   # Inform the admin when a user cancel an account.
   def cancel_account(user_info)
     @user_info = user_info
-    @notify_subject = strip_tags "USER CANCELED ACCOUNT AT #{APPLICATION_CONFIG['name']}"
+    @notify_subject = strip_tags "USER CANCELED ACCOUNT AT #{ENV['APPLICATION_CONFIG_name']}"
     mail( :to => ENV['APPLICATION_CONFIG_admin_notification_address'], :subject => @notify_subject)
   end
 
   # Inform the admin if a user confirms an account
   def account_confirmed(user)
-    @notify_subject = strip_tags( "USER CONFIRMED ACCOUNT AT #{APPLICATION_CONFIG['name']}")
+    @notify_subject = strip_tags( "USER CONFIRMED ACCOUNT AT #{ENV['APPLICATION_CONFIG_name']}")
     @user = user
     mail( :to => ENV['APPLICATION_CONFIG_admin_notification_address'], :subject => @notify_subject)
   end
@@ -41,10 +46,10 @@ class Notifications < ActionMailer::Base
     posting   = blog.postings.find(posting_id)
     @blogtitle= blog.title
     @title    = posting.title
-    @username = posting.user.name
+    @username = posting.user.full_name
     @content  = ContentItem::markdown(posting.body).html_safe
     @url      = blog_posting_url(blog,posting)
-    @notify_subject = strip_tags "A NEW POSTING WAS CREATED AT #{APPLICATION_CONFIG['name']}"
+    @notify_subject = strip_tags "A NEW POSTING WAS CREATED AT #{ENV['APPLICATION_CONFIG_name']}"
 
     begin
       # Attach cover picture
@@ -123,14 +128,18 @@ class Notifications < ActionMailer::Base
     @cart = cart
     @user = user
     @order = order
+    time_slot_info = TimeSlot.find(cart.items.first.time_slot_id).longer_title
     mail( :from => 'messenger@fitwit.com',
-          :to   => 'info@fitwit.com',
-          :subject => "#{user.certain_name} has just signed up for a camp" )
+          :to   => 'management@fitwit.com',
+          :subject => "New Camper: #{time_slot_info},#{@order.amount/100}" )
   end
 
   def inform_customer_about_their_new_journey(user,cart)
     @cart = cart
     @user = user
+    ['FitWitFitnessCampManual120301.pdf','FitWitNutritionGuide.pdf','GeneralWaiverandRelease2012.pdf'].each do |a|
+     attachments[a] = File.read("#{Rails.root}/public/#{a}" )
+    end
     mail( :from => 'messenger@fitwit.com',
           :to   => user.email,
           :subject => "Registration confirmed from FitWit.com" )
