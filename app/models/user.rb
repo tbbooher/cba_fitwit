7331c5968174dee09124daa43ed4ea7f6bd59a36
tim booher <tim@theboohers.org>
2012-10-08 15:19:39 -0400

    got members working

--- app/models/user.rb
+++ app/models/user.rb
@@ -242,6 +242,7 @@ class User
   scope :with_role, lambda { |role| { :where => {:roles_mask.gte => ROLES.index(role) } } }
 
   scope :last_name_first, order_by([:last_name => :asc], [:first_name => :asc])
+  scope :camps_than_name, order_by([:location_id => :asc], [:first_name => :asc])
 
   #def time_slots
   #  self.registrations.map(&:orders).map(&:time_slots)
